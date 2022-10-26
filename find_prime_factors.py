b7873dfad05170d0e6146accc02856d8a7380eea
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-10-26 23:10:57 +0100

    modified the prime factors ex

--- find_prime_factors.py
+++ find_prime_factors.py
@@ -6,9 +6,11 @@ num = int(input('What number would you like to find the prime factors of?:'))
 
 prime_factors = []
 
-def get_prime_factors():
-    if num % num == 0 and num % 1 == 0:
+def get_prime_factors(num):
+    if num % num == 1 and num % 1 == num:
         prime_factors.append(num)
         print(prime_factors)
     else:
         return ('Not a prime number')
+
+get_prime_factors(num)
