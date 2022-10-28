4a311ab0ae560d7b5a8af176fa1c19346512e91b
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-10-28 15:23:50 +0100

    finished prime factors ex

--- find_prime_factors.py
+++ find_prime_factors.py
@@ -4,13 +4,16 @@
 
 num = int(input('What number would you like to find the prime factors of?:'))
 
-prime_factors = []
+def get_prime_factors(a):
+    prime_factors = []
+    divisor = 2
+    while(divisor <= a):
+        if (a % divisor) == 0:
+            prime_factors.append(divisor)
+            a = a//divisor
+        else:
+            divisor += 1
+    return prime_factors
 
-def get_prime_factors(num):
-    if num % num == 1 and num % 1 == num:
-        prime_factors.append(num)
-        print(prime_factors)
-    else:
-        return ('Not a prime number')
-
-get_prime_factors(num)
+b = get_prime_factors(num)
+print(b)
