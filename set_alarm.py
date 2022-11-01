cdd53cd78a65c02f2f56d6fa3903cac6a9a8d3a3
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-11-01 18:04:50 +0000

    tried to do alarm exercise

---
+++ set_alarm.py
@@ -0,0 +1,10 @@
+#write a function to print a message and play a specified sound at a set time
+# 3 inputs - alarm time, sound file and message
+
+import time
+import sched
+import winsound as ws
+
+def alarm(alarm_time, sound_file, message):
+    print('You set your alarm for', )
+    s.run()
